1de637d83b5e2a5555e30ca9ac7edfeaa6ed85d8
Gonzalo Di Berardino <gonzadiber@gmail.com>
2018-10-30 15:54:38 -0300

    Add info accounts

--- config/services.php
+++ config/services.php
@@ -35,4 +35,17 @@ return [
         'secret' => env('STRIPE_SECRET'),
     ],
 
+    //socialite
+    'github' => [
+        'client_id' => env('GITHUB_CLIENT_ID'),
+        'client_secret' => env('GITHUB_CLIENT_SECRET'),
+        'redirect' => env('/login/github/callback'),
+    ],
+
+    'facebook' => [
+        'client_id' => env('FACEBOOK_CLIENT_ID'),
+        'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
+        'redirect' => env('/login/facebook/callback'),
+    ],
+
 ];
