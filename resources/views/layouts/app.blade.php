c31adefee348720de595f7a18d3fd13a20d68f4a
Gonzalo Di Berardino <gonzadiber@gmail.com>
2018-11-01 14:50:48 -0300

    Add social buttons to Login and Register page, routes and login methods

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,12 +12,10 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Slabo+27px" rel="stylesheet">
 </head>
 <body>
     <div id="app">
