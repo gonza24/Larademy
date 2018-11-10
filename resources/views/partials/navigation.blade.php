e799d8a3fbbd537c4bc2cc67794e41b0ff1ce3f1
Gonzalo Di Berardino <gonzadiber@gmail.com>
2018-11-10 19:40:03 -0300

    Add navbar header

---
+++ resources/views/partials/navigation.blade.php
@@ -0,0 +1,23 @@
+<header>
+    <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+        <div class="container">
+
+            <a class="navbar-brand" href="{{ url('/') }}">
+                {{ config('app.name')  }}
+            </a>
+            
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            
+            <div class="collapse navbar-collapse" id="navbarContent">
+                <ul class="navbar-nav mr-auto">
+
+                </ul>
+                <ul class="navbar-nav ml-auto">
+                    
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
